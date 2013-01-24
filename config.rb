5b46ff66a9a7daf283eec575ea777a6fd73628f1
Priit Hansen <priit.hansen@gmail.com>
2013-01-24 16:01:13 +0200

    Initial merge of simple theme and foundation framework

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require 'zurb-foundation'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
